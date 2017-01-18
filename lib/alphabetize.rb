e7a13b1269858756faef4dfa87e93d8cf428a956
John Shelby <jsnd11@gmail.com>
2017-01-17 19:09:02 -0500

    alpha

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -1,3 +1,9 @@
-def alphabetize(arr)
-  # code here
+def alphabetize(array)
+  alphabet = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+  custom_sort = ->(a, b) do
+    a.split('').each_with_index do |char, i|
+      return alphabet.index(a[i]) <=> alphabet.index(b[i]) if a[i] != b[i]
+    end
+  end
+  result=array.sort(&custom_sort)
 end
